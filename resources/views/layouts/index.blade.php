943492e66fa3d210bb2ad4c9fcfabb540eb7bc77
wipkaka/decor <wipkaka@gmail.com>
2021-08-25 10:56:13 +0700

    commit

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -9,7 +9,7 @@
         <link href="https://valleyparty.vn/upload/company/favicon-15320605862.png" rel="shortcut icon" type="image/x-icon">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
-        <link rel="stylesheet" href="css/wstyle.css">
+        <link rel="stylesheet" href="css/style.css">
         <link rel="stylesheet" href="admin_asset/dist/css/jquery-confirm.min.css">
         <title>Valley Party Decor</title>
     </head>
